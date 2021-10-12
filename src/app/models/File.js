4c1dd99e543d0988effc2139e6f3eb3fa08ab20a
Pedro <ispedromartins@gmail.com>
2021-10-12 20:10:18 -0300

    cria a estrutura de agendamento

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -16,10 +16,14 @@ class File extends Model {
         sequelize,
       });
 
-
-
     return this;
   }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' })
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' })
+  }
+
 }
 
 export default File;
---
+++ src/database/migrations/20211012230019-create-appointments.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    return await queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+
+    });
+
+  },
+
+  down: async (queryInterface) => {
+
+
+    return await queryInterface.dropTable('appointments');
+
+  }
+};
