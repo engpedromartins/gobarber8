7e76350ec10d8abd28ab477d8e16bbf4b253e335
Pedro <ispedromartins@gmail.com>
2021-10-12 23:26:54 -0300

    cria o envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,22 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "b94a60fc9aaaa8",
+    pass: "9b56558703ccd4"
+  },
+  deafult: {
+    from: 'TESTANU <noreply@teste.com>'
+  }
+}
+
+//TIPOS DE SERVICOS QUE DISPARAM EMAIL
+// Amazon ses
+// Mailgun
+// Sparkpost
+// gmail
+
+//só em ambiente de dev
+
+// mailtrap (DEV)
